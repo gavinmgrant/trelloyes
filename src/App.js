fe8bbf7efdde6bcaaf5c3bbc0ba8519d14cbba74
Gavin Grant <gavingrant@gmail.com>
2020-07-02 17:38:14 -0700

    added functionality to the delete button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import List from './composition/List'; 
 import './App.css';
+import STORE from './STORE';
+
+function omit(obj, keyToOmit) {
+  return Object.entries(obj).reduce(
+    (newObj, [key, value]) =>
+        key === keyToOmit ? newObj : {...newObj, [key]: value},
+    {}
+  );
+}
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      list: [],
-      allCards: [],
-    }
+  state = {
+    store: STORE,
+  };
+
+  handleDeleteCard = (cardId) => {
+    const { lists, allCards } = this.state.store;
+
+    const newLists = lists.map(list => ({
+      ...list,
+      cardIds: list.cardIds.filter(id => id !== cardId)
+    }));
+
+    const newCards = omit(allCards, cardId);
+
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: newCards
+      }
+    })
   };
 
   render() {
-    const { store } = this.props;
+    const { store } = this.state;
     
     return (
       <main className='App'>
@@ -22,8 +46,10 @@ class App extends Component {
           {store.lists.map(list => (
             <List
               key={list.id}
+              id={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
+              onClickDelete={this.handleDeleteCard}
             />
           ))}
         </div>
